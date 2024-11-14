b5ed999e6550a11cf6ff212565a42080a7b3b012
Alan Lin <linaizhong@gmail.com>
2024-11-15 10:23:05 +1100

    Remove d3 functions

--- frontend/src/d3-examples/NswEducationalDocument/Views/ac_ma_codes.js
+++
@@ -1,8 +0,0 @@
-export const macodes = [
-  {name: 'ACMMG274', value: 'ACMMG274'},
-  {name: 'ACMSP278', value: 'ACMSP278'},
-  {name: 'ACMMG065', value: 'ACMMG065'}, 
-  // {name: '', value: ''}, 
-  {name: 'ACMMG023', value: 'ACMMG023'} 
-]
-  
--- frontend/src/d3-examples/NswEducationalDocument/Views/ac_sc_codes.js
+++
@@ -1,8 +0,0 @@
-export const sccodes = [
-  {name: 'ACSIS203', value: 'ACSIS203'},
-  {name: 'ACSIS148', value: 'ACSIS148'},
-  {name: 'ACSIS206', value: 'ACSIS206'}, 
-  // {name: '', value: ''}, 
-  {name: 'ACSIS139', value: 'ACSIS139'} 
-]
-  
